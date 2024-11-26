aaa0610648d8b2405e4e2bef2803e3ab00004329
James Truty <jtruty@gmail.com>
2024-11-25 19:27:41 -0600

    add support for 4o models

--- src/limits.ts
+++ src/limits.ts
@@ -15,9 +15,18 @@ export class TokenLimits {
     } else if (model === 'gpt-4') {
       this.maxTokens = 8000
       this.responseTokens = 2000
+    } else if (model === 'gpt-4-turbo') {
+      this.maxTokens = 128000
+      this.responseTokens = 4000
+    } else if (model === 'gpt-4o-mini') {
+      this.maxTokens = 128000
+      this.responseTokens = 4000
+    } else if (model === 'gpt-4o') {
+      this.maxTokens = 128000
+      this.responseTokens = 4000
     } else {
-      this.maxTokens = 4000
-      this.responseTokens = 1000
+      this.maxTokens = 128000
+      this.responseTokens = 4000
     }
     // provide some margin for the request tokens
     this.requestTokens = this.maxTokens - this.responseTokens - 100
